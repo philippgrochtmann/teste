cdff1d4dd64016742819a2e22602fa33e0ff687c
philippgrochtmann <108080149+philippgrochtmann@users.noreply.github.com>
2022-06-30 19:06:35 -0300

    arquivo

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Points(levels));
+            Console.ReadLine();
+            int tempo = 5;
+
+        }
+        
+        static int Points(int levels)
+        {
+            if (levels == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return levels + Points(levels - 1);
+            }
+            
+
+
+            
+
+        }
+
+        
+
+    }
+
+
+
+}
